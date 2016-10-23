784ab8b1bb0fb423b4e188cd64d5a7012fde403a
Cameron Probert <c.probert@hotmail.co.nz>
2016-10-23 20:55:07 +0900

    Created the mod
    Added .modinfo file and Rules.sql

---
+++ Cheaper Unit Upgrade/Rules.sql
@@ -0,0 +1,13 @@
+/*
+	Less annoying AI
+	by Cameron Probert (2016)
+*/
+ 
+
+-----------------------------------------------
+-- Upgrade Costs
+-----------------------------------------------
+
+UPDATE GlobalParamaters SET UPGRADE_BASE_COST = 10;
+UPDATE GlobalParamaters SET UPGRADE_MINIMUM_COST = 20;
+UPDATE GlobalParamaters SET UPGRADE_NET_PRODUCTION_PERCENT_COST = 45;
