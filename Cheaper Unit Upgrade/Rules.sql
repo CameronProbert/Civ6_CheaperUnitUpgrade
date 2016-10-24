8394627987397754ffec46c7ad3ceed9b25ccbb7
Cameron Probert <c.probert@hotmail.co.nz>
2016-10-24 20:09:54 +0900

    Changed values to dynamic 2/3 rather than static values

--- Cheaper Unit Upgrade/Rules.sql
+++ Cheaper Unit Upgrade/Rules.sql
@@ -1,6 +1,8 @@
 /*
-	Less annoying AI
+	Cheaper Unit Upgrade
 	by Cameron Probert (2016)
+	
+	Changes Upgrade Costs to 2/3 of vanilla.
 */
  
 
@@ -8,6 +10,9 @@
 -- Upgrade Costs
 -----------------------------------------------
 
-UPDATE GlobalParamaters SET UPGRADE_BASE_COST = 10;
-UPDATE GlobalParamaters SET UPGRADE_MINIMUM_COST = 20;
-UPDATE GlobalParamaters SET UPGRADE_NET_PRODUCTION_PERCENT_COST = 45;
+UPDATE GlobalParamaters 
+	SET UPGRADE_BASE_COST = UPGRADE_BASE_COST/2*3;--10;
+UPDATE GlobalParamaters 
+	SET UPGRADE_MINIMUM_COST = UPGRADE_MINIMUM_COST/2*3;--20;
+UPDATE GlobalParamaters 
+	SET UPGRADE_NET_PRODUCTION_PERCENT_COST = UPGRADE_NET_PRODUCTION_PERCENT_COST/2*3;--45;
